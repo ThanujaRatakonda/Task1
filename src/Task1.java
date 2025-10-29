8e0ad91d9fa54b5895cf602016c8eb3e75ce2222
THANUJA <124047527+ThanujaRatakonda@users.noreply.github.com>
2025-10-29 08:31:44 +0000

    Update Task1.java
    v1

--- src/Task1.java
+++ src/Task1.java
@@ -1,5 +1,5 @@
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello from Task1!,Hi from Task1");
+        System.out.println("Hello Task1");
     }
 }
